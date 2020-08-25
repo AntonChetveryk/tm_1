696c0b7d4a43d2ad9dc6f17a487c2ccd638044f0
AntonChetveryk <chetverykanton92@gmail.com>
2020-08-25 12:53:44 +0300

    Updates

---
+++ precache-manifest.9f8172b8b1e529b162cfab5bbbcfe702.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e012b14ed8b63863aff2acf46ca161",
+    "url": "/tm_1/index.html"
+  },
+  {
+    "revision": "4664a6367c0be99a3fe1",
+    "url": "/tm_1/static/js/main.06be4ed6.chunk.js"
+  },
+  {
+    "revision": "d6a50053d6e1c9dfec7d",
+    "url": "/tm_1/static/js/runtime-main.92f68cf2.js"
+  }
+]);
---
+++ static/js/main.06be4ed6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptm_1=this.webpackJsonptm_1||[]).push([[0],[function(o,n,s){o.exports=s(1)},function(o,n){console.log("hello")}],[[0,1]]]);
+//# sourceMappingURL=main.06be4ed6.chunk.js.map
